3a0fdb9d93aca4c2682e20a753524b9c4ba410ba
Harshit Kumar Gupta <harshit.knit@gmail.com>
2020-06-17 17:40:26 +0000

    optmizing

--- main.py
+++ main.py
@@ -12,23 +12,12 @@ async def root():
 
 @app.get("/analyze-text/text")
 async def grab_text(url: str = None):
-
-    article_text = grab_text_from_article(url)
-    return {'article_text': article_text}
+    return {'article_text': grab_text_from_article(url)}
 
 @app.get("/analyze-text/ner")
 async def analyze_ner(url: str = None):
-
-    article_text = grab_text_from_article(url)
-    ner = ner_with_counts(article_text)
-
-    items = collections.Counter(str(tuple(item)) for item in ner)
-
-    return {'Entities': items}
+    return {'Entities': collections.Counter(str(tuple(item)) for item in ner_with_counts(grab_text_from_article(url)))}
 
 @app.get("/analyze-text/pos")
 async def analyze_pos(url: str):
-    article_text = grab_text_from_article(url)
-    pos = pos_with_lemma(article_text)
-
-    return {"Parts of Speech": pos}
+    return {"Parts of Speech": pos_with_lemma(grab_text_from_article(url))}
